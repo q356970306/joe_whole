60c27e5f93cea62d85e828a75daeb302fa4085b0
qiaojinshuai <q356970306@163.com>
2018-09-15 10:57:59 +0800

    回滚

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World,my name is zhuliqing&qiaojinshuai!");
+        System.out.println("Hello World!");
     }
 }
